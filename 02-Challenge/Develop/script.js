8bccd97db1aaf4472795f2be4d96d7e06c003e1e
scelsic2 <scelsic2@gmail.com>
2023-03-12 14:52:50 -0400

    fixed code with comments

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -11,30 +11,44 @@ var generateBtn = document.querySelector("#generate");
 
 
 // Write password to the #password input
-function writePassword() {
-   var correctPrompt = getPrompt(); 
-  var passwordText = document.querySelector("#password");
+// function writePassword() {
+//    var correctPrompt = getPrompt(); 
+//   var passwordText = document.querySelector("#password");
 
-    if(correctPrompt) {
-      var newPassword = generatePassword();
-      passwordText.value = newPassword;
+//     if(correctPrompt) {
+//       var newPassword = generatePassword();<-- You wrote a function to collect an array based on user choices (getPrompt).
+// Then you wrote a function to loop through that array and select items (generatePassword).
+// You want to generatePassword based on your getPrompt, so you have to pass getPrompt into your generatePassword fuction so that generatePassword can start working with that data.
+// If you don't pass anything into it, generatePassword doesn't have any data to work with.  See line 33.
+//       passwordText.value = newPassword;
    
-    }  else {
-      passwordText.value = "";
-    } 
+//     }  else {
+//       passwordText.value = "";
+//     } 
       
+// }
+
+function writePassword() {
+  var correctPrompt = getPrompt();
+  var newPassword = generatePassword(correctPrompt);
+  var passwordText = document.querySelector("#password");
+  passwordText.value = newPassword;
 }
 
 function generatePassword() {
-    var password = "";
-    for(var i = 0; i < characterLength; i++) {
-        var randomIndex = Math.floor(Math.random() * choiceArr.length);
-        password = password + choiceArr[randomIndex];
-    }
-    return password;
+  var password = "";
+  for(var i = 0; i < characterLength; i++) {
+      var randomIndex = Math.floor(Math.random() * choiceArr.length);
+      password = password + choiceArr[randomIndex];
+  }
+  return password; 
 
-}  
+}
 
+// You created your empty array choiceArr at the top of your function.
+// The function then adds items into the array with each step.
+// In order to get the array you just built out of the fuction, you have to reutrn the array choiceArr.
+// You had it set to return "true."
 function getPrompt(){
   choiceArr=[];
   characterLength = parseInt(prompt("How many characters would you like your password to be? (8-128 characters)"));
@@ -61,14 +75,15 @@ function getPrompt(){
   
   }
   
-  return true;
+  return choiceArr;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+  
 
 
-
-function generatePassword() {
-}
+// You had these 2 lines below live in your code which was breaking it.
+// function generatePassword() {
+// }
 
