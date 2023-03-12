3f62ee526a15009301da1bceda12bbf6f30a0d31
Swhitty44 <swhitt4444@gmail.com>
2023-03-12 12:39:10 -0400

    added code to generate a password. added in all the characters i want availbe for the password. also added in alerts for how someone wants their password written with the appropriate amount of characters available between 8-128.

---
+++ 02-Challenge/Develop/script.js
@@ -0,0 +1,75 @@
+var characterLength = 8;
+var choiceArr = [];
+
+var specialChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-','=','+','[',']', '{','}','/','|']
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCase = ['A', 'B', 'C', 'D', 'E','F', 'G', 'H', 'I', 'J', 'K', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W','X', 'Y', 'Z'];
+var numbers =['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var lowerCase 
+
+// Write password to the #password input
+function writePassword() {
+   var correctPrompt = getPrompt(); 
+
+    if(correctPrompt) {
+       var newPassword = generatePassword();
+       var passwordText = document.querySelector("#password");
+
+      passwordText.value = newPassword;
+   
+    }  else {
+      passwordText.value = "";
+    } 
+      
+}
+
+function generatePassword() {
+    var password = "";
+    for(var i = 0; i < characterLength; i++) {
+        var randomIndex = Math.floor(Math.random() * choiceArr.length);
+        password = password + choiceArr[randomIndex];
+    }
+    return password;
+
+}  
+
+function getPrompt(){
+  choiceArr=[];
+  characterLength = parseInt(prompt("How many characters would you like your password to be? (8-128 characters)"));
+
+  if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+    alert("character length has to be a number, 8 - 128 digits. Please try again.");
+    return false;
+  }
+
+  if (confirm("would you like lowercase letters in your password?")) {
+    choiceArr = choiceArr.concat(lowerCase); 
+  }
+
+   if (confirm("would you like uppercase letters in your password?")) {
+    choiceArr = choiceArr.concat(upperCase); 
+  }
+
+  if (confirm("would you like special characters in your password?")) {
+    choiceArr = choiceArr.concat(specialChar); 
+  }
+
+  if (confirm("would you like numbers in your password?")) {
+    choiceArr = choiceArr.concat(numbers); 
+  
+  }
+  
+  return true;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
+function generatePassword() {
+}
+
