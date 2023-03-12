43f394ee55cc2c37bc94657b21f3c0cfc8f492ab
Swhitty44 <swhitt4444@gmail.com>
2023-03-12 13:06:59 -0400

    making sure everything is saved and up to date for final touches later on

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -8,16 +8,15 @@ var numbers =['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lowerCase 
+
 
 // Write password to the #password input
 function writePassword() {
    var correctPrompt = getPrompt(); 
+  var passwordText = document.querySelector("#password");
 
     if(correctPrompt) {
-       var newPassword = generatePassword();
-       var passwordText = document.querySelector("#password");
-
+      var newPassword = generatePassword();
       passwordText.value = newPassword;
    
     }  else {
